c4f9d84afb5d5a4a7e5ab9fa7dee2abc675ccb13
Yakiv Koliada <yakov.kolyada@gmail.com>
2025-09-08 00:04:58 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
-};
+function check(str, bracketsConfig = []) {
+  if (bracketsConfig[0].length % 2 !== 0 || typeof str !== 'string') {
+    return false;
+  }
+
+  const openBrackets = bracketsConfig.map((item) => item[0]);
+  const closeBrackets = bracketsConfig.map((item) => item[1]);
+
+  let parsedStr = str;
+  let found = true;
+  while (found) {
+    found = false;
+    for (let i = 0; i < bracketsConfig.length; i += 1) {
+      while (parsedStr.includes(openBrackets[i] + closeBrackets[i])) {
+        parsedStr = parsedStr.replace(openBrackets[i] + closeBrackets[i], '');
+        found = true;
+      }
+    }
+  }
+
+  return parsedStr.length === 0;
+}
+
+module.exports = check;
+
+// console.log(check('()', [['(', ')']])) // -> true
+// console.log(check('((()))()', [['(', ')']])) // -> true
+// console.log(check('())(', [['(', ')']])) // -> false
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']])) // -> true
+// console.log(check('[(])', [['(', ')'], ['[', ']']])) // -> false
+// console.log(check('[]()', [['(', ')'], ['[', ']']])) // -> true
+// console.log(check('[]()(', [['(', ')'], ['[', ']']])) // -> false
+// console.log(check('||||', [['|', '|']])) // -> true
+// console.log(check('||||', [['|', '|']])) // -> true
+// console.log(check('([)]', [['(', ')'], ['[', ']']])) // -> false
